43e3a9d86f23da6cbd98d9ba5de166c1897680e2
foovo <vm0012@student.uni-lj.si>
2021-02-23 18:22:18 +0100

    Added function that prints grav. accelaration and height.

--- v1/gravitacija.py
+++ v1/gravitacija.py
@@ -1,27 +1,31 @@
-
 import sys
 
 C = 6.674*(10**-11)
 M = 5.972*(10**24)
 r = 6.371*(10**6)
 
-visina = 0
 
 def main():
     if (len(sys.argv) > 2):
         print("Too many arguments.")
         sys.exit()
     if(len(sys.argv) == 1):
-        print("Please add argument 'nadmorska visina'.")
+        print("Please add argument 'height'.")
         sys.exit()
     
-    visina = sys.argv[1]
+    height = float(sys.argv[1])
     print("OIS je zakon!")
 
+    print_data(height)
 
-def grav_posp():
-    return (C*M)/((r+visina)**2)
 
+def gravitational_accelaration(height):
+    return (C*M)/((r+height)**2)
+
+def print_data(height):
+    print("Height is", height)
+
+    print("Gravitational accelaration is", gravitational_accelaration(height))
 
 if __name__ == "__main__":
     main()
