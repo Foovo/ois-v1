7d8017d8238844148e195aada321db90e448cde4
foovo <vm0012@student.uni-lj.si>
2021-02-23 18:07:21 +0100

    Added gravitacijski pospesek

--- v1/gravitacija.py
+++ v1/gravitacija.py
@@ -1,6 +1,26 @@
+
+import sys
+
+C = 6.674*(10**-11)
+M = 5.972*(10**24)
+r = 6.371*(10**6)
+
+visina = 0
+
 def main():
+    if len(sys.argv) > 2:
+        print("Too many arguments.")
+        sys.exit()
+    if(len sys.argv == 1):
+        print("Please add argument 'nadmorska visina'.")
+        sys.exit()
+    
+    visina = sys.argv[1]
     print("OIS je zakon!")
 
+
+def grav_posp():
+    return (C*M)/((r+visina)**2)
 
 
 if __name__ == "__main__":
